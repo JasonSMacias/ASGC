3ef5602087ee8a2587cde5df6435d53dc74e7db0
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-03-06 15:31:13 -0500

    Installed react-router-dom, imported it.  set up react public index w/ bulma and google fonts, basic structure w/ routes.  added to folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Heading from './pages/dashboard/';
 
 import './App.css';
 
 class App extends Component {
   state = { users: []}
+
+// to get users: {this.state.users.map(user => <li key={user.id}>{user.user_name}, {user.address}</li>)}
 
   componentDidMount() {
     fetch('api/users')
@@ -13,14 +17,9 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <h1>Users</h1>
-        <ol>
-        {this.state.users.map(user => 
-          <li key={user.id}>{user.user_name}, {user.address}</li>
-        )}
-        </ol>
-      </div>
+      <Route>
+
+      </Route>
     );
   }
 }
