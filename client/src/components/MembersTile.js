abc432e468d275b7abd1296b872bc4ef9645954d
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-03-09 12:29:17 -0500

    More basic styling on pages

--- client/src/components/MembersTile.js
+++ client/src/components/MembersTile.js
@@ -5,6 +5,33 @@ function MembersTile(props) {
   return (
     <section>
         <div className="box">
+          {/* <div class="dropdown is-active is-inline">
+            <div class="dropdown-trigger">
+              <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                <span><i class="fas fa-bars"></i></span>
+              </button>
+            </div>
+            <div class="dropdown-menu" id="dropdown-menu" role="menu">
+              <div class="dropdown-content">
+                <a href="#" class="dropdown-item">
+                  Dropdown item
+                </a>
+                <a class="dropdown-item">
+                  Other dropdown item
+                </a>
+                <a href="#" class="dropdown-item is-active">
+                  Active dropdown item
+                </a>
+                <a href="#" class="dropdown-item">
+                  Other dropdown item
+                </a>
+                <hr class="dropdown-divider" />
+                <a href="#" class="dropdown-item">
+                  With a divider
+                </a>
+              </div>
+            </div>
+          </div> */}
           <h2 className="section-head has-text-centered"><span className="icon has-text-black-bis"><i class="fas fa-brain"></i></span> <Link to='/members' className="lnk">Members</Link></h2> <hr />
           <p>This will have general info with a hamburger menu upper left</p>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem minus ea possimus debitis maxime minima, consequatur fuga natus rem dignissimos adipisci quasi pariatur vero sint, dolore voluptate excepturi nostrum ducimus.</p>
--- client/src/pages/GameSites.js
+++ client/src/pages/GameSites.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function GameSites(props) {
   return (
-    <h1>Game Sites Page</h1>
+    <div className="box" >
+      <h2 className="section-head"><span className="icon has-text-black-bis"><i class="fas fa-chess"></i></span >  <span className="pagehead">Game Sites</span></h2> <hr />
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt magnam dolorem exercitationem illum vel similique voluptatum pariatur repellat blanditiis dicta quisquam laborum excepturi eius, maiores id nemo itaque nobis alias?</p>
+    </div>
   );
 }
 
