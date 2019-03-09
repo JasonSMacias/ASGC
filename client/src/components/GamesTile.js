0e75b75a9a8e87d128aa391b54387b93cabc610c
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-03-09 12:05:28 -0500

    set up main page tiles to link to individual pages corresponding to them.  Styling.

--- client/src/components/GamesTile.js
+++ client/src/components/GamesTile.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function GamesTile(props) {
   return (
     <section>
     
       <div className="box" >
-        <h2 className="section-head has-text-centered"><span className="icon has-text-black-bis"><i class="fas fa-chess"></i></span >  <span className="lnk">Games</span></h2> <hr />
+        <h2 className="section-head has-text-centered"><span className="icon has-text-black-bis"><i class="fas fa-chess"></i></span >  <Link to='/games' className="lnk">Games</Link></h2> <hr />
         <p>This will have general info with a hamburger menu upper left</p>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus unde illum nulla voluptate necessitatibus libero accusantium impedit eaque vero. Odio quasi voluptates corrupti laudantium modi inventore voluptas repellendus voluptatibus voluptatem.</p>
       </div>
--- client/src/components/MembersTile.js
+++ client/src/components/MembersTile.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function MembersTile(props) {
   return (
     <section>
         <div className="box">
-          <h2 className="section-head has-text-centered"><span className="icon has-text-black-bis"><i class="fas fa-brain"></i></span> Members</h2> <hr />
+          <h2 className="section-head has-text-centered"><span className="icon has-text-black-bis"><i class="fas fa-brain"></i></span> <Link to='/members' className="lnk">Members</Link></h2> <hr />
           <p>This will have general info with a hamburger menu upper left</p>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem minus ea possimus debitis maxime minima, consequatur fuga natus rem dignissimos adipisci quasi pariatur vero sint, dolore voluptate excepturi nostrum ducimus.</p>
         </div>
